d4460d6caa27ee227e0bdf0e387e8531f1daaad6
Tushar Nagar <tusharrock146@gmail.com>
2025-03-20 20:46:01 +0530

    Create index.js
    Constants: demo data

---
+++ data/index.js
@@ -0,0 +1,14 @@
+import { Bot } from "lucide-react";
+
+const demoIcons = {
+    bot: <Bot className="w-6 h-6" />,
+};
+
+export const demos = [
+  {
+    title: "Bot Detection",
+    description: "dentify and block unwanted bot traffic while allowing legitimate bots.",
+    href: "/demo/bot-detection",
+    icon: demoIcons.bot,
+  }
+];
