82ba5477313d690c95693d2fd258bb85eb0b8acf
Tushar Nagar <tusharrock146@gmail.com>
2025-03-21 11:27:20 +0530

    Update layout and add new demo to demos list

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { Card } from "@/components/Card";
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <div className="max-w-5xl mx-auto py-16 px-4 sm:py-24 sm:px-6">
-        <div className="text-center space-y-4 mb-16">
+      <div className="max-w-5xl mx-auto py-12 px-4 sm:py-16 sm:px-6">
+        <div className="text-center space-y-4 mb-12">
           <h1 className="text-4xl font-extrabold tracking-tight text-gray-100 sm:text-5xl md:text-6xl">
             Arcjet Security - Demo
           </h1>
@@ -14,7 +14,7 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:gap-12">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:gap-5">
           {demos.map((demo) => (
             <Card key={demo.title} {...demo} />
           ))}
--- data/index.js
+++ data/index.js
@@ -1,14 +1,21 @@
-import { Bot } from "lucide-react";
+import { Bot, Gauge } from "lucide-react";
 
 const demoIcons = {
     bot: <Bot className="w-6 h-6" />,
+    gauge: <Gauge className="w-6 h-6" />,
 };
 
 export const demos = [
   {
     title: "Bot Detection",
-    description: "dentify and block unwanted bot traffic while allowing legitimate bots.",
+    description: "Identify and block unwanted bot traffic while allowing legitimate bots.",
     href: "/demo/bot-detection",
     icon: demoIcons.bot,
-  }
+  },
+  {
+    title: "Rate Limiting",
+    description: "Implement flexible rate limiting with token bucket algorithm.",
+    href: "/demo/rate-limit",
+    icon: demoIcons.gauge,
+  },
 ];
