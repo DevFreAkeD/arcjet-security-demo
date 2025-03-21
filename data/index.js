2073cb669f4db30e8cc13f48989a4dafae0c4f76
Tushar Nagar <tusharrock146@gmail.com>
2025-03-21 14:04:57 +0530

    add new demo to demos list

--- data/index.js
+++ data/index.js
@@ -1,9 +1,10 @@
-import { Bot, Gauge, Shield } from "lucide-react";
+import { Bot, Gauge, Mail, Shield } from "lucide-react";
 
 const demoIcons = {
     bot: <Bot className="w-6 h-6" />,
     gauge: <Gauge className="w-6 h-6" />,
     shield: <Shield className="w-6 h-6" />,
+    email: <Mail className="w-6 h-6" />,
 };
 
 export const demos = [
@@ -24,5 +25,11 @@ export const demos = [
     description: "Protect against SQL injection, XSS, and other common web attacks.",
     href: "/demo/shield-waf-protection",
     icon: demoIcons.shield,
+  },
+  {
+    title: "Email Validation",
+    description: "Validate & verify an email address and Prevent fake signups and reduce spam or fraudulent accounts efficiently.",
+    href: "/demo/email-validation",
+    icon: demoIcons.email,
   },
 ];
